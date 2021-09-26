21d84b021b4ec215bb140098960e4e0df7652165
Willem Prins <willemprins@Willems-MacBook-Pro.local>
2021-09-26 16:11:50 +0100

    created tag repositories and db

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -1,6 +1,12 @@
 DROP TABLE merchants;
+DROP TABLE tags;
 
 CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE tags (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
